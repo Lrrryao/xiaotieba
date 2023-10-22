c8d1da5ea8d00199a3be85921b6a9b3b926f7f0e
Lrrryao <131437135+Lrrryao@users.noreply.github.com>
2023-10-22 16:43:14 +0800

    Update cache.go

--- xiaotieba/cache/cache.go
+++ xiaotieba/cache/cache.go
@@ -6,7 +6,7 @@ import (
 )
 
 type CacheStore interface {
-	Get(ctx context.Context, key string) error
+	Get(ctx context.Context, key string) (string, error)
 
 	Add(ctx context.Context, key string, value interface{}, expires time.Duration) error
 	Delete(ctx context.Context, key string) error
